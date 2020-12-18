01fb8cd804248142b0578d9f002826abe7cf8e96
Karsten Stiegler <opitt@web.de>
2020-12-18 22:47:01 +0100

    Day 18 solution

--- day18/day18.py
+++ day18/day18.py
@@ -1,5 +1,5 @@
 # https://adventofcode.com/2020/day/18
-
+from functools import reduce
 import os
 
 def main(day):
@@ -11,17 +11,16 @@ def main(day):
     
     def calculate(e):
         #e.g. (4 * (5 + 9 + 8) + 7) * 6    
-        
+        #find parenthesis and recursively calculate the expression
         while "(" in e:
-            p1 = e.rfind("(")
-            p2 = e.find(")", p1)
-            calc = e[p1+1:p2]
-            val = calculate(calc)
+            p1, p2  = e.rfind("("), e.find(")", p1)
+            val = calculate(e[p1+1:p2])
+            #replace the parenthesis with the calculated value
             e = e[:p1] + str(val) + e[p2+1:]
+        
+        # now only numbers and + and * are in the string, separated by space
         ops = e.split(" ")
-
-        result = int(ops[0])
-        ops = ops[1:] 
+        result, ops = int(ops[0]), ops[1:] 
         while len(ops):
             if ops[0] == "+":
                 result += int(ops[1])
@@ -29,13 +28,37 @@ def main(day):
                 result *= int(ops[1])
             ops = ops[2:]
         return result
-                
+
+    def calculate2(e):
+        #e.g. (4 * (5 + 9 + 8) + 7) * 6    
+        while "(" in e:
+            p1, p2 = e.rfind("("), e.find(")", p1)
+            val = calculate2(e[p1+1:p2])
+            e = e[:p1] + str(val) + e[p2+1:]
+
+        # split by * to calculated first the + operations
+        ops = e.split(" * ")
+        for i, op in enumerate(ops):
+            ops[i] = sum([int(o) for o in op.split(" + ")])
+        # multiply the result
+        result = reduce(lambda x,y: x*y, ops)
+        return result
 
     #! PART 1
     #!
     result = 0
     for expression in calculations:
         result += calculate(expression)
+    print(result)
+
+    #! PART 2
+    #!
+    result = 0
+    for expression in calculations:
+        #expression = "1 + 2 * 3 + 4 * 5 + 6"  #231
+        #expression = "1 + (2 * 3) + (4 * (5 + 6))"
+        #expression = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
+        result += calculate2(expression)
     print(result)
 
 main(18)
