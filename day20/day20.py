dae5c1931303f2670ebd597c623de5109063a4be
Karsten Stiegler <opitt@web.de>
2020-12-20 18:58:46 +0100

    Day 20 part 2 - got the matrix sorted

--- day20/day20.py
+++ day20/day20.py
@@ -42,8 +42,11 @@ def main(day):
         #  int( "#.#.".replace("#", "1").replace(".", "0"),2)
 
     def find_matching_tiles(tile, side):
-        # find a tile in all other tilse, that matches to the side
-        return [tile_name for tile_name, sides in t_sides.items() if tile_name != tile and side in sides]
+        # find a tile in all other tiles, that matches to the side
+        matches = [tile_name for tile_name, sides in t_sides.items() if tile_name != tile and side in sides]
+        if len(matches) > 1:
+            raise RuntimeWarning
+        return "" if len(matches) == 0 else matches[0]
 
     t_match = {}
     for title, tile in t_sides.items():
@@ -60,5 +63,64 @@ def main(day):
             result *= int(title)
     print(f"Result {result}") #17250897231301
 
+    t_match_t = {}
+    for tile, matches in t_match.items():
+        t_match_t[tile] = list(set(matches.values()))
+    
+    def get_tile_with_x(tile,x):
+        for connected_tile in t_match_t[tile].values():
+            if len(t_match_t[connected_tile].values()) == x:
+                return connected_tile
+        return None
+
+    def find_tile_matching(criteria):
+        # criteria ["1212","",""] means
+        # find a tile, that has 3 connections and one of them is 1212
+        for tile, connected_tiles in t_match_t.items():
+            #  "2381": ['1543', '2837', '3761']
+            if len(connected_tiles) == len(criteria):
+                check = [c for c in criteria if c != ""]
+                s = sum([crit in connected_tiles for crit in check])
+                if s == len(check):
+                    return tile
+        return None
+
+    image = [[]]
+    for y in range(12):
+        for x in range(12):
+            image[y].append("")
+        image.append([])
+
+    for y in range(12):
+        for x in range(12):
+            # get the next tile
+            criteria = []
+            if x-1 >= 0:
+                tile_attached = image[y][x - 1]
+                criteria.append(tile_attached)
+            if x + 1 < 12:
+                tile_attached = image[y][x + 1]
+                criteria.append(tile_attached)
+            if y - 1 >= 0:
+                tile_attached = image[y - 1][x]
+                criteria.append(tile_attached)
+            if y + 1 < 12:
+                tile_attached = image[y + 1][x]
+                criteria.append(tile_attached)
+            next_tile = find_tile_matching(criteria)
+            image[y][x] = next_tile
+            for tile in criteria:
+                if tile != "":
+                    # remove connection wit me from the other tile
+                    t_match_t[tile].remove(next_tile)
+                    t_match_t[tile].append("")
+                    # remove the connection to other tile from myself
+                    t_match_t[next_tile].remove(tile)
+                    t_match_t[next_tile].append("")
+    #       t_match_t[next_tile].remove(image[y][x])  # remove the prev connection from the current tile
+        
+
+    print("So what")
+    pass
 
 main(20)
